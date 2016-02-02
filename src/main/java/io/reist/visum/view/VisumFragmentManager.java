7843e8ef0f16f8140a5389076d4281689aaf2847
defuera <denisv@bookmate.com>
2016-02-02 16:40:06 +0300

    Get rid of VisumFragmentActivity move it's functionality to helper class VisumFramentManager

--- src/main/java/io/reist/visum/view/VisumFragmentManager.java
+++ src/main/java/io/reist/visum/view/VisumFragmentManager.java
@@ -3,41 +3,55 @@ package io.reist.visum.view;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-
-import io.reist.visum.R;
 
 /**
  * Created by defuera on 29/01/2016.
  * This Activity helps you handle your fragments
  */
-public class VisumFragmentActivity extends AppCompatActivity implements VisumFragment.FragmentController{
+public class VisumFragmentManager {
 
     /**
      * @param fragment - fragment to display
      * @param remove   - boolean, stays for whether current fragment should be thrown away or stay in a back stack.
      *                 false to stay in a back stack
+     * @param resId    - id of view group to put fragment to
      */
-    @Override
-    public void showFragment(VisumFragment fragment, boolean remove) {
-        showFragment(fragment, remove, false);
+    public static void showFragment(FragmentManager fragmentManager, VisumFragment fragment, @IdRes int resId, boolean remove) {
+        showFragment(fragmentManager, fragment, resId, remove, false);
     }
 
     /**
      * @param popBackStackInclusive all entries up to but not including that entry will be removed
-     * @see VisumFragmentActivity#showFragment
      */
-    protected void showFragment(VisumFragment fragment, boolean remove, boolean popBackStackInclusive) {
-        FragmentManager fragmentManager = getFragmentManager();
+    public static void showFragment(
+            FragmentManager fragmentManager,
+            VisumFragment fragment,
+            @IdRes int resId,
+            boolean remove,
+            boolean popBackStackInclusive
+    ) {
+
         Fragment topmostFragment = findTopmostFragment(fragmentManager);
+
         if (topmostFragment != null && fragment.getName().equals(topmostFragment.getTag())) {
             return;
         }
-        replace(fragmentManager, topmostFragment, fragment, remove, popBackStackInclusive);
+
+        replace(fragmentManager, topmostFragment, fragment, resId, remove, popBackStackInclusive);
+
     }
 
-    private static void replace(FragmentManager fragmentManager, Fragment what, VisumFragment with, boolean remove, boolean popBackStackInclusive) {
+    private static void replace(
+            FragmentManager fragmentManager,
+            Fragment what,
+            VisumFragment with,
+            @IdRes int resId,
+            boolean remove,
+            boolean popBackStackInclusive
+    ) {
+
         if (popBackStackInclusive && fragmentManager.getBackStackEntryCount() > 0) {
             fragmentManager.popBackStackImmediate(fragmentManager.getBackStackEntryAt(0).getName(), FragmentManager.POP_BACK_STACK_INCLUSIVE);
         }
@@ -57,14 +71,14 @@ public class VisumFragmentActivity extends AppCompatActivity implements VisumFra
         if (with.isAdded()) {
             transaction = transaction.show(with);
         } else {
-            transaction = transaction.add(R.id.fragment_container, with, fragmentName);
+            transaction = transaction.add(resId, with, fragmentName);
         }
 
         transaction.addToBackStack(fragmentName).commit();
     }
 
     @Nullable
-    protected static Fragment findTopmostFragment(FragmentManager fragmentManager) {
+    public static Fragment findTopmostFragment(FragmentManager fragmentManager) {
         int backStackEntryCount = fragmentManager.getBackStackEntryCount();
         Fragment topmostFragment;
         if (backStackEntryCount > 0) {
@@ -75,4 +89,5 @@ public class VisumFragmentActivity extends AppCompatActivity implements VisumFra
         }
         return topmostFragment;
     }
+
 }

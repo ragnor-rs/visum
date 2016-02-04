cdcc7c5f56a3c5441a857885bb120588451bde86
defuera <denisv@bookmate.com>
2016-02-04 15:54:20 +0300

    ass visum android service

---
+++ src/main/java/io/reist/visum/VisumAndroidService.java
@@ -0,0 +1,55 @@
+package io.reist.visum;
+
+import android.app.Service;
+
+import io.reist.visum.view.VisumClient;
+
+/**
+ * Created by defuera on 02/02/2016.
+ */
+public abstract class VisumAndroidService extends Service implements VisumClient {
+
+    private Long componentId;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onCreate() {
+        inject(getComponent());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getComponentCache().invalidateComponentFor(this);
+    }
+
+    private ComponentCache getComponentCache() {
+        ComponentCacheProvider application = (ComponentCacheProvider) getApplicationContext();
+        return application.getComponentCache();
+    }
+
+    //region VisumClient
+
+    @Override
+    public Long getComponentId() {
+        return componentId;
+    }
+
+    @Override
+    public void setComponentId(Long componentId) {
+        this.componentId = componentId;
+    }
+
+    @Override
+    public Object getComponent() {
+        if (getComponentCache() != null) {
+            return getComponentCache().getComponentFor(this);
+        } else {
+            return null;
+        }
+    }
+
+    //endregion
+
+}
---
+++ src/main/java/io/reist/visum/VisumIntentService.java
@@ -0,0 +1,65 @@
+package io.reist.visum;
+
+import android.app.IntentService;
+
+import io.reist.visum.view.VisumClient;
+
+/**
+ * Created by defuera on 02/02/2016.
+ */
+public abstract class VisumIntentService extends IntentService implements VisumClient {
+
+    private Long componentId;
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public VisumIntentService(String name) {
+        super(name);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onCreate() {
+        inject(getComponent());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getComponentCache().invalidateComponentFor(this);
+    }
+
+    private ComponentCache getComponentCache() {
+        ComponentCacheProvider application = (ComponentCacheProvider) getApplicationContext();
+        return application.getComponentCache();
+    }
+
+    //region VisumView
+
+    @Override
+    public Long getComponentId() {
+        return componentId;
+    }
+
+    @Override
+    public void setComponentId(Long componentId) {
+        this.componentId = componentId;
+    }
+
+    @Override
+    public Object getComponent() {
+        if (getComponentCache() != null) {
+            return getComponentCache().getComponentFor(this);
+        } else {
+            return null;
+        }
+    }
+
+    //endregion
+
+}
+

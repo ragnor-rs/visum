cddec30394559bd796a506a6e64a332a3533b37a
Alexey Petrushin <petrushinalex@gmail.com>
2016-11-28 17:24:50 +0300

    refactoring

--- demo/src/test/java/io/reist/sandbox/core/RobolectricTestCase.java
+++ demo/src/test/java/io/reist/sandbox/core/RobolectricTestCase.java
@@ -21,28 +21,32 @@ public class RobolectricTestCase {
         ShadowLog.stream = System.out;
     }
 
+    private static final RxAndroidSchedulersHook rxAndroidSchedulersHook = new RxAndroidSchedulersHook() {
+        @Override
+        public Scheduler getMainThreadScheduler() {
+            return Schedulers.immediate();
+        }
+    };
+
+    private static final RxJavaSchedulersHook rxJavaSchedulersHook = new RxJavaSchedulersHook() {
+        @Override
+        public Scheduler getIOScheduler() {
+            return Schedulers.immediate();
+        }
+
+        @Override
+        public Scheduler getNewThreadScheduler() {
+            return Schedulers.immediate();
+        }
+    };
+
     @BeforeClass
     public static void registerRxHooks() {
         RxAndroidPlugins.getInstance().reset();
-        RxAndroidPlugins.getInstance().registerSchedulersHook(new RxAndroidSchedulersHook() {
-            @Override
-            public Scheduler getMainThreadScheduler() {
-                return Schedulers.immediate();
-            }
-        });
+        RxAndroidPlugins.getInstance().registerSchedulersHook(rxAndroidSchedulersHook);
 
         RxJavaPlugins.getInstance().reset();
-        RxJavaPlugins.getInstance().registerSchedulersHook(new RxJavaSchedulersHook() {
-            @Override
-            public Scheduler getIOScheduler() {
-                return Schedulers.immediate();
-            }
-
-            @Override
-            public Scheduler getNewThreadScheduler() {
-                return Schedulers.immediate();
-            }
-        });
+        RxJavaPlugins.getInstance().registerSchedulersHook(rxJavaSchedulersHook);
     }
 
     @AfterClass
